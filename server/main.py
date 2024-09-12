b21eb9e331d9bbdfa9a8d379092aa59f554d3be2
Jake Funk <93415355+Jake-Funk@users.noreply.github.com>
2024-09-12 18:49:01 -0400

    Added the game initialization endpoint. (#21)
    * added endpoint for creating a new game and storing it.

--- server/main.py
+++ server/main.py
@@ -1,8 +1,23 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+import uuid
+from util.game_state import GameState
 
 app = FastAPI()
+games = {}
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+class NewGameRequest(BaseModel):
+    num_players: int
+
+
+@app.post("/new_game")
+async def initialize_game(req: NewGameRequest) -> str:
+    try:
+        temp = GameState(req.num_players)
+    except:
+        raise HTTPException(status_code=403, detail="Invalid number of players.")
+
+    key = str(uuid.uuid4())
+    games[key] = temp
+    return key
