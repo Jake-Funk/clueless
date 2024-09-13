489803e4e4ba98f8c5ee3621508df0145dc478fd
EthanBayer <bayeres1871@gmail.com>
2024-09-13 17:00:31 -0400

    Bug fixes after testing server endpoint

--- server/main.py
+++ server/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI, HTTPException
 
 from util.game_state import GameState
-from util.enums import MoveAction, HallEnum, HttpEnum
+from util.enums import MoveAction, HallEnum, RoomEnum, HttpEnum
 from util.functions import get_player_location
 from util.movement import Map, move_player, validate_move
 
@@ -31,14 +31,14 @@ async def initialize_game(req: NewGameRequest) -> str:
 @app.post("/")
 async def move(movement: MoveAction):
     # Check if player can be found on current map
-    try:
-        current_location = get_player_location(movement.player)
-    except Exception:
-        raise HTTPException(status_code=404, detail="Player not found on Map.")
-
     if movement.id == None or movement.id not in games.keys():
         raise HTTPException(status_code=404, detail="Game not found.")
     key = movement.id
+
+    try:
+        current_location = get_player_location(movement.player, games[key])
+    except Exception:
+        raise HTTPException(status_code=404, detail="Player not found on Map.")
 
     http_code = validate_move(movement, current_location, games[key])
 
@@ -46,11 +46,11 @@ async def move(movement: MoveAction):
         move_player(movement, current_location, games[key])
         # Check if player entered a room
         if isinstance(movement.location, RoomEnum):
-            games[key].current_phase.turn = "suggest"
+            games[key].current_turn.phase = "suggest"
         # TODO: If not a room, need to keep turn as move but change
         # game player to be the next one. Would like utility function for this
         return {
-            "Response": f"Successfully moved {movement.player.value} to {movement.location}"
+            "Response": f"Successfully moved {movement.player.value} to {movement.location.value}"
         }
     else:
         raise HTTPException(status_code=http_code[0].value, detail=http_code[1])
