39ba2721a999c4f3228027979f25f8e7ebd39b44
Jake <jakedfunk@hotmail.com>
2024-09-22 16:13:26 -0400

    testing fetch

--- client/clueless_web_ui/app/new_game/page.tsx
+++ client/clueless_web_ui/app/new_game/page.tsx
@@ -1,9 +1,58 @@
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import { Github } from "lucide-react";
-import Image from "next/image";
+"use client"
+import { Button } from "@/components/ui/button"
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { Github } from "lucide-react"
+import Image from "next/image"
+import { useState } from "react"
+import { useForm } from "react-hook-form"
+import { z } from "zod"
+
+const formSchema = z.object({
+  numPlayers: z.coerce
+    .number()
+    .min(2, { message: "You need at least 2 people to play" })
+    .max(6, { message: "You can only play with up to 6 players." }),
+})
 
 export default function Home() {
+//   const [gameCode, setGameCode] = useState("")
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      numPlayers: 2,
+    },
+  })
+
+  async function onSubmit(values: z.infer<typeof formSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    const rawResp = await fetch(
+      "https://clueless-server-915069415929.us-east1.run.app/new_game",
+      {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ num_players: values.numPlayers }),
+      }
+    )
+    const content = await rawResp.json()
+
+    console.log(content)
+    console.log(values)
+  }
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] animate-in ease-in-out zoom-in-0 duration-1000">
       <main className="flex flex-col gap-8 row-start-2 items-center justify-center sm:items-start ">
@@ -12,14 +61,29 @@ export default function Home() {
             How many people will be playing?
           </h1>
         </div>
-
-        <Input
-          type={"number"}
-          max={6}
-          min={2}
-          className="w-1/2 m-auto sm:w-auto"
-        />
-        <Button className="m-auto">Start Game</Button>
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(onSubmit)}
+            className="flex flex-col m-auto"
+          >
+            <FormField
+              control={form.control}
+              name="numPlayers"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel className="m-auto">Number of Players</FormLabel>
+                  <FormControl>
+                    <Input {...field} type="number" />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button type="submit" className="m-3">
+              Start Game
+            </Button>
+          </form>
+        </Form>
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
         <a
@@ -48,5 +112,5 @@ export default function Home() {
         </a>
       </footer>
     </div>
-  );
+  )
 }
