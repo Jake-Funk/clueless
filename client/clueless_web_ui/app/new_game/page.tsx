f9de1bb0e3acbfd439865b22e085d75e47ea8066
Jake <jakedfunk@hotmail.com>
2024-09-21 20:40:07 -0400

    added animations to the homepage

---
+++ client/clueless_web_ui/app/new_game/page.tsx
@@ -0,0 +1,52 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Github } from "lucide-react";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] animate-in ease-in-out zoom-in-0 duration-1000">
+      <main className="flex flex-col gap-8 row-start-2 items-center justify-center sm:items-start ">
+        <div className="flex items-center gap-4">
+          <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl text-center">
+            How many people will be playing?
+          </h1>
+        </div>
+
+        <Input
+          type={"number"}
+          max={6}
+          min={2}
+          className="w-1/2 m-auto sm:w-auto"
+        />
+        <Button className="m-auto">Start Game</Button>
+      </main>
+      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
+        <a
+          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+          href="#"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            aria-hidden
+            src="https://nextjs.org/icons/file.svg"
+            alt="File icon"
+            width={16}
+            height={16}
+          />
+          Rules
+        </a>
+        <a
+          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+          href="#"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Github width={16} height={16} />
+          GitHub
+        </a>
+      </footer>
+    </div>
+  );
+}
