066ddaf609a9e725e9dbd0743d25a729e794fe50
Jake <jakedfunk@hotmail.com>
2024-10-22 17:30:30 -0400

    added option to choose player when joining a game

--- client/clueless_web_ui/app/join_game/page.tsx
+++ client/clueless_web_ui/app/join_game/page.tsx
@@ -1,7 +1,7 @@
-"use client";
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import { BookMarked, Github } from "lucide-react";
+"use client"
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+import { BookMarked, Github } from "lucide-react"
 import {
   Form,
   FormControl,
@@ -9,27 +9,38 @@ import {
   FormItem,
   FormLabel,
   FormMessage,
-} from "@/components/ui/form";
-import { useRouter } from "next/navigation";
-import { useForm } from "react-hook-form";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { z } from "zod";
-import { useState } from "react";
+} from "@/components/ui/form"
+import { useRouter } from "next/navigation"
+import { useForm } from "react-hook-form"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { z } from "zod"
+import { useState } from "react"
 
 const formSchema = z.object({
   id: z.coerce.string().uuid("Not a valid game ID"),
-});
+})
 
 export default function Home() {
-  const router = useRouter();
-  const [err, setErr] = useState(false);
+  const router = useRouter()
+  const [err, setErr] = useState(false)
+  const [playerSelect, setPlayerSelect] = useState(false)
+  const [playerOptions, setPlayerOptions] = useState<string[]>([])
+
+  function chosePlayer(player: string) {
+    try {
+      localStorage.setItem("player", player)
+    } catch {
+      console.error("Problem adding data to local storage")
+    }
+    router.push("/play")
+  }
 
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
       id: "",
     },
-  });
+  })
 
   async function onSubmit(values: z.infer<typeof formSchema>) {
     // ✅ This will be type-safe and validated.
@@ -41,66 +52,88 @@ export default function Home() {
           Accept: "application/json",
           "Content-Type": "application/json",
         },
-      },
-    );
+      }
+    )
     if (rawResp.status == 200) {
-      const content = await rawResp.json();
+      const content = await rawResp.json()
 
-      console.log(content);
+      console.log(content)
       try {
-        localStorage.setItem("gameID", values.id);
-        router.push("/play");
+        localStorage.setItem("gameID", values.id)
+        setPlayerSelect(true)
+        setPlayerOptions(Object.keys(content.player_character_mapping))
       } catch {
-        console.error("Err adding the game ID to local storage");
+        console.error("Err adding the game ID to local storage")
       }
     } else {
-      setErr(true);
+      setErr(true)
     }
   }
 
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] animate-in ease-in-out zoom-in-0 duration-1000">
-      <main className="flex flex-col gap-8 row-start-2 items-center justify-center sm:items-start ">
-        <div className="flex items-center gap-4">
-          <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl text-center">
-            Enter the game code to join:
-          </h1>
-        </div>
-        <Form {...form}>
-          <form
-            onSubmit={form.handleSubmit(onSubmit)}
-            className="flex flex-col m-auto"
-          >
-            <FormField
-              control={form.control}
-              name="id"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel className="m-auto">Game ID</FormLabel>
-                  <FormControl>
-                    <Input
-                      {...field}
-                      onFocus={() => {
-                        setErr(false);
-                      }}
-                    />
-                  </FormControl>
-                  {err ? (
-                    <FormMessage>
-                      This server did not find this game ID.
-                    </FormMessage>
-                  ) : (
-                    <FormMessage />
-                  )}
-                </FormItem>
-              )}
-            />
-            <Button type="submit" className="m-3">
-              Join Game
-            </Button>
-          </form>
-        </Form>
-      </main>
+      {!playerSelect ? (
+        <main className="flex flex-col gap-8 row-start-2 items-center justify-center sm:items-start ">
+          <div className="flex items-center gap-4">
+            <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl text-center">
+              Enter the game code to join:
+            </h1>
+          </div>
+          <Form {...form}>
+            <form
+              onSubmit={form.handleSubmit(onSubmit)}
+              className="flex flex-col m-auto"
+            >
+              <FormField
+                control={form.control}
+                name="id"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel className="m-auto">Game ID</FormLabel>
+                    <FormControl>
+                      <Input
+                        {...field}
+                        onFocus={() => {
+                          setErr(false)
+                        }}
+                      />
+                    </FormControl>
+                    {err ? (
+                      <FormMessage>
+                        This server did not find this game ID.
+                      </FormMessage>
+                    ) : (
+                      <FormMessage />
+                    )}
+                  </FormItem>
+                )}
+              />
+              <Button type="submit" className="m-3">
+                Join Game
+              </Button>
+            </form>
+          </Form>
+        </main>
+      ) : (
+        <main className="flex flex-col gap-8 row-start-2 items-center justify-center sm:items-start ">
+          <div className="self-center font-bold text-xl">
+            Who are you playing as?
+          </div>
+          <div className="flex flex-wrap gap-4 justify-center">
+            {playerOptions.map((item) => {
+              return (
+                <Button
+                  key={item}
+                  className="basis-1/3"
+                  onClick={() => {chosePlayer(item)}}
+                >
+                  {item}
+                </Button>
+              )
+            })}
+          </div>
+        </main>
+      )}
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
@@ -122,5 +155,5 @@ export default function Home() {
         </a>
       </footer>
     </div>
-  );
+  )
 }
