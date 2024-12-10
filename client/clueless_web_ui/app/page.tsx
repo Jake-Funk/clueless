ed9eae24749c7a8b9dd35c518ef5ccfd9c99a548
Ethan Bayer <bayeres1871@gmail.com>
2024-12-09 20:45:05 -0500

    Add Basic Overall README and Fix Suggestion Option Bug (#102)
    * Add Basic Overall README and fix bug
    
    * added the github link to all the github buttons
    
    ---------
    
    Co-authored-by: Jake <jakedfunk@hotmail.com>

--- client/clueless_web_ui/app/page.tsx
+++ client/clueless_web_ui/app/page.tsx
@@ -75,7 +75,7 @@ export default function Home() {
         </a>
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="#"
+          href="https://github.com/Jake-Funk/clueless"
           target="_blank"
           rel="noopener noreferrer"
         >
