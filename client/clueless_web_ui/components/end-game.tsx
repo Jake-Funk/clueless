42150edf0b23a76f903117d21e6b370f9e5e8964
Jake <jakedfunk@hotmail.com>
2024-12-09 20:55:11 -0500

    added rules pdf

--- client/clueless_web_ui/components/end-game.tsx
+++ client/clueless_web_ui/components/end-game.tsx
@@ -67,7 +67,7 @@ export default function EndGame() {
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="#"
+          href="/Clue-Less.pdf"
           target="_blank"
           rel="noopener noreferrer"
         >
